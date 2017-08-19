0bebf0ec99423e1acc59e52b8e304900024de9a9
Francesco Scali <francesco.scali@gmail.com>
2017-08-19 17:55:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+//Heroku can Inject environment variables, that is runtime configurations, like the PORT we are running on!
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000);
