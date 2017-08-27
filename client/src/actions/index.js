9dfbf05f79dbf7dd36a524135fc51ede3c2804f6
Francesco Scali <francesco.scali@gmail.com>
2017-08-27 18:24:26 +0200

    client side: components skeleton and api call to check if loggedin

---
+++ client/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { FETCH_USER } from "./types";
+
+//this ES6 syntax means that the fetchUser returns a function with a dispatch argument. That function contains an await statement, so we
+//put the async keyword in front of the function
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user");
+  dispatch({
+    type: FETCH_USER,
+    payload: res
+  });
+
+  //because in the index.js we configured the redux-thunk middleware, that library will automatically
+  //inspect every action creator, and if the action creator returns a function, like this one, it will automatically
+  //inject the dispatch function inside the function so that we can use it
+};
